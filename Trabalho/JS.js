0262b3417e02221cead2ca07ef2f41993db40d27
Tulio24 <127336278+Tulio24@users.noreply.github.com>
2023-12-20 19:14:24 -0300

    Add files via upload

---
+++ Trabalho/JS.js
@@ -0,0 +1,591 @@
+function mudaCor(el) {
+    var cor = el.style.backgroundColor;
+    if (cor == 'rgb(0, 0, 255)') {
+        el.style.backgroundColor = '#adb5bd';
+    } else {
+        el.style.backgroundColor = '#adb5bd';
+    }
+}
+
+//------------------------------------------------------------- 
+
+let mensagem1 = document.querySelector(".mensagem-1");
+
+
+function showMessage1() {
+    mensagem1.style.display = "block";
+}
+
+function hideMessage1() {
+    mensagem1.style.display = "none";
+}
+
+var btn1 = document.querySelector('#primeiro-botao');
+var container1 = document.querySelector('.container-1');
+btn1.addEventListener('click', function () {
+
+    if (container1.style.display === 'block') {
+        container1.style.display = 'none'
+    } else {
+        container1.style.display = 'block';
+    }
+
+});
+
+// -------------------------------------------------------
+
+let mensagem2 = document.querySelector(".mensagem-2");
+
+
+function showMessage2() {
+    mensagem2.style.display = "block";
+}
+
+function hideMessage2() {
+    mensagem2.style.display = "none";
+}
+
+var btn2 = document.querySelector('#segundo-botao');
+var container2 = document.querySelector('.container-2');
+btn2.addEventListener('click', function () {
+
+    if (container2.style.display === 'block') {
+        container2.style.display = 'none'
+    } else {
+        container2.style.display = 'block';
+    }
+
+});
+
+// -------------------------------------------------------------
+
+let mensagem3 = document.querySelector(".mensagem-3");
+
+
+function showMessage3() {
+    mensagem3.style.display = "block";
+}
+
+function hideMessage3() {
+    mensagem3.style.display = "none";
+}
+
+var btn3 = document.querySelector('#terceiro-botao');
+var container3 = document.querySelector('.container-3');
+btn3.addEventListener('click', function () {
+
+    if (container3.style.display === 'block') {
+        container3.style.display = 'none'
+    } else {
+        container3.style.display = 'block';
+    }
+
+});
+
+// --------------------------------------------------------------
+
+let mensagem = document.querySelector(".mensagem");
+
+
+function showMessage() {
+    mensagem.style.display = "block";
+}
+
+function hideMessage() {
+    mensagem.style.display = "none";
+}
+
+var btn = document.querySelector('#botao');
+var container = document.querySelector('.container');
+btn.addEventListener('click', function () {
+
+    if (container.style.display === 'block') {
+        container.style.display = 'none'
+    } else {
+        container.style.display = 'block';
+    }
+
+});
+
+// -------------------------------------------------------------
+
+let mensagem4 = document.querySelector(".mensagem-4");
+
+
+function showMessage4() {
+    mensagem4.style.display = "block";
+}
+
+function hideMessage4() {
+    mensagem4.style.display = "none";
+}
+
+var btn4 = document.querySelector('#botao-4');
+var container4 = document.querySelector('.container-4');
+btn4.addEventListener('click', function () {
+
+    if (container4.style.display === 'block') {
+        container4.style.display = 'none'
+    } else {
+        container4.style.display = 'block';
+    }
+
+});
+// ------------------------------------------------------------
+
+let mensagem5 = document.querySelector(".mensagem-5");
+
+function showMessage5() {
+    mensagem5.style.display = "block";
+}
+
+function hideMessage5() {
+    mensagem5.style.display = "none";
+}
+
+var btn5 = document.querySelector('#botao-5');
+var container5 = document.querySelector('.container-5');
+btn5.addEventListener('click', function () {
+
+    if (container5.style.display === 'block') {
+        container5.style.display = 'none'
+    } else {
+        container5.style.display = 'block';
+    }
+
+});
+
+// ------------------------------------------------------------
+
+let mensagem6 = document.querySelector(".mensagem-6");
+
+function showMessage6() {
+    mensagem6.style.display = "block";
+}
+
+function hideMessage6() {
+    mensagem6.style.display = "none";
+}
+
+var btn6 = document.querySelector('#botao-6');
+var container6 = document.querySelector('.container-6');
+btn6.addEventListener('click', function () {
+
+    if (container6.style.display === 'block') {
+        container6.style.display = 'none'
+    } else {
+        container6.style.display = 'block';
+    }
+
+});
+
+// ------------------------------------------------------------
+
+let mensagem7 = document.querySelector(".mensagem-7");
+
+function showMessage7() {
+    mensagem7.style.display = "block";
+}
+
+function hideMessage7() {
+    mensagem7.style.display = "none";
+}
+
+var btn7 = document.querySelector('#botao-7');
+var container7 = document.querySelector('.container-7');
+btn7.addEventListener('click', function () {
+
+    if (container7.style.display === 'block') {
+        container7.style.display = 'none'
+    } else {
+        container7.style.display = 'block';
+    }
+
+});
+
+// ------------------------------------------------------------
+
+let mensagem8 = document.querySelector(".mensagem-8");
+
+function showMessage8() {
+    mensagem8.style.display = "block";
+}
+
+function hideMessage8() {
+    mensagem8.style.display = "none";
+}
+
+var btn8 = document.querySelector('#botao-8');
+var container8 = document.querySelector('.container-8');
+btn8.addEventListener('click', function () {
+
+    if (container8.style.display === 'block') {
+        container8.style.display = 'none'
+    } else {
+        container8.style.display = 'block';
+    }
+
+});
+
+//   ------------------------------------------------------------
+
+let mensagem9 = document.querySelector(".mensagem-9");
+
+function showMessage9() {
+    mensagem9.style.display = "block";
+}
+
+function hideMessage9() {
+    mensagem9.style.display = "none";
+}
+
+var btn9 = document.querySelector('#botao-9');
+var container9 = document.querySelector('.container-9');
+btn9.addEventListener('click', function () {
+
+    if (container9.style.display === 'block') {
+        container9.style.display = 'none'
+    } else {
+        container9.style.display = 'block';
+    }
+
+});
+
+//   ------------------------------------------------------------
+
+let mensagem10 = document.querySelector(".mensagem-10");
+
+function showMessage10() {
+    mensagem10.style.display = "block";
+}
+
+function hideMessage10() {
+    mensagem10.style.display = "none";
+}
+
+var btn10 = document.querySelector('#botao-10');
+var container10 = document.querySelector('.container-10');
+btn10.addEventListener('click', function () {
+
+    if (container10.style.display === 'block') {
+        container10.style.display = 'none'
+    } else {
+        container10.style.display = 'block';
+    }
+
+});
+
+//   ------------------------------------------------------------
+
+let mensagem11 = document.querySelector(".mensagem-11");
+
+function showMessage11() {
+    mensagem11.style.display = "block";
+}
+
+function hideMessage11() {
+    mensagem11.style.display = "none";
+}
+
+var btn11 = document.querySelector('#botao-11');
+var container11 = document.querySelector('.container-11');
+btn11.addEventListener('click', function () {
+
+    if (container11.style.display === 'block') {
+        container11.style.display = 'none'
+    } else {
+        container11.style.display = 'block';
+    }
+
+});
+
+//   --------------------------------------------------------------
+
+let mensagem12 = document.querySelector(".mensagem-12");
+
+function showMessage12() {
+    mensagem12.style.display = "block";
+}
+
+function hideMessage12() {
+    mensagem12.style.display = "none";
+}
+
+var btn12 = document.querySelector('#botao-12');
+var container12 = document.querySelector('.container-12');
+btn12.addEventListener('click', function () {
+
+    if (container12.style.display === 'block') {
+        container12.style.display = 'none'
+    } else {
+        container12.style.display = 'block';
+    }
+
+});
+
+//   --------------------------------------------------------------
+
+let mensagem13 = document.querySelector(".mensagem-13");
+
+function showMessage13() {
+    mensagem13.style.display = "block";
+}
+
+function hideMessage13() {
+    mensagem13.style.display = "none";
+}
+
+var btn13 = document.querySelector('#botao-13');
+var container13 = document.querySelector('.container-13');
+btn13.addEventListener('click', function () {
+
+    if (container13.style.display === 'block') {
+        container13.style.display = 'none'
+    } else {
+        container13.style.display = 'block';
+    }
+
+});
+
+//   --------------------------------------------------------------
+
+let mensagem14 = document.querySelector(".mensagem-14");
+
+function showMessage14() {
+    mensagem14.style.display = "block";
+}
+
+function hideMessage14() {
+    mensagem14.style.display = "none";
+}
+
+var btn14 = document.querySelector('#botao-14');
+var container14 = document.querySelector('.container-14');
+btn14.addEventListener('click', function () {
+
+    if (container14.style.display === 'block') {
+        container14.style.display = 'none'
+    } else {
+        container14.style.display = 'block';
+    }
+
+});
+
+//   --------------------------------------------------------------
+
+let mensagem15 = document.querySelector(".mensagem-15");
+
+function showMessage15() {
+    mensagem15.style.display = "block";
+}
+
+function hideMessage15() {
+    mensagem15.style.display = "none";
+}
+
+var btn15 = document.querySelector('#botao-15');
+var container15 = document.querySelector('.container-15');
+btn15.addEventListener('click', function () {
+
+    if (container15.style.display === 'block') {
+        container15.style.display = 'none'
+    } else {
+        container15.style.display = 'block';
+    }
+
+});
+
+//   --------------------------------------------------------------
+
+let mensagem16 = document.querySelector(".mensagem-16");
+
+function showMessage16() {
+    mensagem16.style.display = "block";
+}
+
+function hideMessage16() {
+    mensagem16.style.display = "none";
+}
+
+var btn16 = document.querySelector('#botao-16');
+var container16 = document.querySelector('.container-16');
+btn16.addEventListener('click', function () {
+
+    if (container16.style.display === 'block') {
+        container16.style.display = 'none'
+    } else {
+        container16.style.display = 'block';
+    }
+
+});
+
+//   --------------------------------------------------------------
+
+let mensagem17 = document.querySelector(".mensagem-17");
+
+function showMessage17() {
+    mensagem17.style.display = "block";
+}
+
+function hideMessage17() {
+    mensagem17.style.display = "none";
+}
+
+var btn17 = document.querySelector('#botao-17');
+var container17 = document.querySelector('.container-17');
+btn17.addEventListener('click', function () {
+
+    if (container17.style.display === 'block') {
+        container17.style.display = 'none'
+    } else {
+        container17.style.display = 'block';
+    }
+
+});
+
+//  -------------------------------------------------------------
+
+let mensagem18 = document.querySelector(".mensagem-18");
+
+function showMessage18() {
+    mensagem18.style.display = "block";
+}
+
+function hideMessage18() {
+    mensagem18.style.display = "none";
+}
+
+var btn18 = document.querySelector('#botao-18');
+var container18 = document.querySelector('.container-18');
+btn18.addEventListener('click', function () {
+
+    if (container18.style.display === 'block') {
+        container18.style.display = 'none'
+    } else {
+        container18.style.display = 'block';
+    }
+
+});
+
+//  ------------------------------------------------------------
+
+let mensagem19 = document.querySelector(".mensagem-19");
+
+function showMessage19() {
+    mensagem19.style.display = "block";
+}
+
+function hideMessage19() {
+    mensagem19.style.display = "none";
+}
+
+var btn19 = document.querySelector('#botao-19');
+var container19 = document.querySelector('.container-19');
+btn19.addEventListener('click', function () {
+
+    if (container19.style.display === 'block') {
+        container19.style.display = 'none'
+    } else {
+        container19.style.display = 'block';
+    }
+
+});
+
+//  ------------------------------------------------------------
+
+let mensagem20 = document.querySelector(".mensagem-20");
+
+function showMessage20() {
+    mensagem20.style.display = "block";
+}
+
+function hideMessage20() {
+    mensagem20.style.display = "none";
+}
+
+var btn20 = document.querySelector('#botao-20');
+var container20 = document.querySelector('.container-20');
+btn20.addEventListener('click', function () {
+
+    if (container20.style.display === 'block') {
+        container20.style.display = 'none'
+    } else {
+        container20.style.display = 'block';
+    }
+
+});
+
+//  ------------------------------------------------------------
+
+let mensagem21 = document.querySelector(".mensagem-21");
+
+function showMessage21() {
+    mensagem21.style.display = "block";
+}
+
+function hideMessage21() {
+    mensagem21.style.display = "none";
+}
+
+var btn21 = document.querySelector('#botao-21');
+var container21 = document.querySelector('.container-21');
+btn21.addEventListener('click', function () {
+
+    if (container21.style.display === 'block') {
+        container21.style.display = 'none'
+    } else {
+        container21.style.display = 'block';
+    }
+
+});
+
+//  -----------------------------------------------------------
+
+let mensagem22 = document.querySelector(".mensagem-22");
+
+function showMessage22() {
+    mensagem22.style.display = "block";
+}
+
+function hideMessage22() {
+    mensagem22.style.display = "none";
+}
+
+var btn22 = document.querySelector('#botao-22');
+var container22 = document.querySelector('.container-22');
+btn22.addEventListener('click', function () {
+
+    if (container22.style.display === 'block') {
+        container22.style.display = 'none'
+    } else {
+        container22.style.display = 'block';
+    }
+
+});
+
+//  -----------------------------------------------------------
+
+
+let mensagem23 = document.querySelector(".mensagem-23");
+
+function showMessage23() {
+    mensagem23.style.display = "block";
+}
+
+function hideMessage23() {
+    mensagem23.style.display = "none";
+}
+
+var btn23 = document.querySelector('#botao-23');
+var container23 = document.querySelector('.container-23');
+btn23.addEventListener('click', function () {
+
+    if (container23.style.display === 'block') {
+        container23.style.display = 'none'
+    } else {
+        container23.style.display = 'block';
+    }
+
+});
+
+// ----------------------------------------------------------------
